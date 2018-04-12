047ce73588afdf06af5a016cea352b5dc68a2ac0
louliying <51669811@qq.com>
2018-04-12 13:41:23 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,13 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const extractTextPlugin = require('extract-text-webpack-plugin');
-const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
-const ZipPlugin = require('zip-webpack-plugin');
-
-// webpack ftp上传， 它依赖deploy-kit
-const DeployPlugin = require('deploy-kit/plugins/ftp-webpack-plugin')
 module.exports = {
     entry: {
-        libs:['./src/js/mobile-base.js', './src/js/jquery_1_8_3.js', './src/js/Ecar.dialogPrompt.js', './src/js/Ecar.popWin.js'],
-        index: ['./src/js/index.js'],
-        // data: ['./src/mock/data.js'],
-        // result: './src/js/result.js',
-
-        // vendor: ["babel-polyfill"]
+        libs:['./src/js/mobile-base.js', './src/js/jquery_1_8_3.js'],
+        index: ['./src/js/css.js', './src/js/index.js'],
     },
+    // sourcemap功能
     devtool: 'inline-source-map',
     // 将库的对象挂靠在全局对象中，
     // 通过另外一个对象存储对象名以及映射到对应模块名的变量，
@@ -35,6 +27,7 @@ module.exports = {
         contentBase: './dist'
     },
     plugins:[
+        // 清空dist目录
         new CleanWebpackPlugin([__dirname + '/dist']),
         // HtmlWebpackPlugin 默认会生成一个index.html，即使本来已经存在
         // 会把所有..bundle.js都自动 添加 到生成的index.html里
@@ -48,57 +41,18 @@ module.exports = {
              // 这个模板对应上面那个节点
             chunks: [ "libs",'index']
         }),
-        // 拆分插件
-        /*new webpack.optimize.CommonsChunkPlugin({
-            // name: ["vendor", "runtime"] // 上面入口定义的节点组
-            // filename:'libs.[chunkhash].js' //最后生成的文件名
-            name: ['libs'],
-            filename: 'libs.[chunkhash].js'
-        }),*/
+
         // css抽取
         // contenthash是内容是
         // 因webpack是把CSS打包在js里的，所以contenthash，只适合于css
         new extractTextPlugin("index.[contenthash].css"),
 
-        // 把webpack打包的的文件，进行zip包
-        new ZipPlugin({
-            // zip包生成的目录
-            path: path.resolve(__dirname, 'dist'),
-            // zip包的名字
-            filename: 'dist.zip',
-            fileOptions: {
-                mtime: new Date(),
-                mode: parseInt("0100664", 8),
-                compress: true,
-                forceZip64Format: false
-            }
-
-        }),
-
-        // ftp 上传
-        /*new DeployPlugin({
-            server: 'zhangweiming:Pass1234@10.47.59.207:21',
-            // deploy all files in the directory
-            workspace: __dirname + '/dist',
-            // where the files are placed on the server
-            deployTo: '/ftpUp',
-            /*rules: [
-                {
-                  test: /zips\/(.*\.zip)$/,
-                  dest: 'ftpUp/zips/[$1]'
-                }
-            ]*/
-        // })*/
 
     ],
     module:{
         rules: [
             {
                 test: /\.css$/,
-                /*use: [
-                    'style-loader',
-                    'css-loader'
-                ]*/
                 use: extractTextPlugin.extract({
                     fallback: "style-loader",
                     use: "css-loader"
@@ -132,8 +86,3 @@ module.exports = {
     }
   };
 
-
-  // http://www.cnblogs.com/ihardcoder/p/5623411.html
-  // http://foio.github.io/wepack-code-spliting/
-  // https://www.npmjs.com/package/deploy-kit
-  // https://github.com/erikdesjardins/zip-webpack-plugin/blob/master/index.js
